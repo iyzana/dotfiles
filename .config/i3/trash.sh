7e5cc4d91e3a50ca3a6ee2c89a5bac2f5dc5810f
iyzana <16743652+iyzana@users.noreply.github.com>
2022-01-27 18:34:27 +0100

    i3: add support for text in datatrash script

--- .config/i3/trash.sh
+++ .config/i3/trash.sh
@@ -1,12 +1,29 @@
 #!/bin/env bash
 
-xclip -selection clipboard -out -target image/jpg \
-    | curl \
-        --form "file=@-;filename=screenshot-$(date +'%Y-%m-%dT%H%M%S').jpg" \
-        --form 'keep_for=86400' \
-        --form "password=$DATATRASH_PASSWORD" \
-        https://trash.randomerror.de/upload \
-    | tr --delete '\n' \
-    | xclip -selection clipboard
-notify-send 'datatrash' 'upload complete'
-
+targets=$(xclip -selection clipboard -out -target TARGETS)
+if echo "$targets" | grep 'image/png'; then
+    filename="screenshot-$(date +'%Y-%m-%dT%H%M%S').png"
+    xclip -selection clipboard -out -target 'image/png' \
+        | curl \
+            --form "file=@-;filename=$filename" \
+            --form 'keep_for=86400' \
+            --form "password=$DATATRASH_PASSWORD" \
+            https://trash.randomerror.de/upload \
+        | tr --delete '\n' \
+        | xclip -selection clipboard
+    notify-send 'datatrash' 'upload complete'
+elif echo "$targets" | grep 'text/uri-list'; then
+    notify-send 'datatrash' 'unsupported clipboard format'
+elif echo "$targets" | grep 'UTF8_STRING'; then
+    xclip -selection clipboard -out -target 'UTF8_STRING' \
+        | curl \
+            --form "text=@-" \
+            --form 'keep_for=86400' \
+            --form "password=$DATATRASH_PASSWORD" \
+            https://trash.randomerror.de/upload \
+        | tr --delete '\n' \
+        | xclip -selection clipboard
+    notify-send 'datatrash' 'upload complete'
+else
+    notify-send 'datatrash' 'unsupported clipboard format'
+fi
