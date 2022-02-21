8bf8b35f7e7793a168d59e04a374af3bd0cd4936
iyzana <16743652+iyzana@users.noreply.github.com>
2022-02-21 23:44:51 +0100

    i3: use clipboard instead of primary for generating qr codes

--- .config/i3/qr.sh
+++ .config/i3/qr.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 file="/tmp/qr-$(date).png"
-qrencode "$(xclip -selection primary -out)" -o "$file" \
+qrencode "$(xclip -selection clipboard -out)" -o "$file" \
     || convert -background black -fill white -pointsize 24 -font Roboto label:'Too large' "$file"
 eog -n "$file"
 rm "$file"
