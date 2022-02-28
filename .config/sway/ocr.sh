7e7a54d813da0b306ec4d37b6809bdbb7defd71d
iyzana <16743652+iyzana@users.noreply.github.com>
2022-02-28 14:44:30 +0100

    sway: add ocr shortcut

---
+++ .config/sway/ocr.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+targets=$(wl-paste -l)
+if echo "$targets" | grep 'image/png'; then
+    image_file="/tmp/ocr-image-$(date --iso-8601=seconds)"
+    text_file="/tmp/ocr-text-$(date --iso-8601=seconds)"
+    wl-paste > "$image_file"
+    languages=$(
+        find /usr/share/tessdata -maxdepth 1 -type f -name '*.traineddata' -print0 | xargs --null --replace=_ --max-args=1 basename _ .traineddata
+    )
+    lang=$(echo "$languages" | rofi -dmenu -p "language")
+    tesseract -l "$lang" "$image_file" "$text_file"
+    wl-copy < "$text_file.txt"
+    rm "$image_file" "$text_file.txt"
+    notify-send 'ocr' "$lang text copied to clipboard"
+else
+    notify-send 'ocr' 'unsupported clipboard format'
+fi
