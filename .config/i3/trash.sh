4033130e258260d929592839d267ebed330f2d9f
iyzana <16743652+iyzana@users.noreply.github.com>
2022-03-20 21:48:25 +0100

    i3: add support for uploading files to trash

--- .config/i3/trash.sh
+++ .config/i3/trash.sh
@@ -13,7 +13,25 @@ if echo "$targets" | grep 'image/png'; then
         | xclip -selection clipboard
     notify-send 'datatrash' 'upload complete'
 elif echo "$targets" | grep 'text/uri-list'; then
-    notify-send 'datatrash' 'unsupported clipboard format'
+    file_uris=$(xclip -selection clipboard -out -target 'text/uri-list')
+    if [[ "$(echo "$file_uris" | wc -l)" != 1 ]]; then
+        notify-send 'datatrash' 'more than one element selected'
+        exit
+    fi
+    if [[ ! "$file_uris" =~ ^file://.*$ ]]; then
+        notify-send 'datatrash' 'selection is not a file'
+        exit
+    fi
+    # strip file://
+    file_path=$(echo "$file_uris" | tail --bytes=+8) 
+    curl \
+        --form "file=@$file_path" \
+        --form 'keep_for=86400' \
+        --form "password=$DATATRASH_PASSWORD" \
+        https://trash.randomerror.de/upload \
+        | tr --delete '\n' \
+        | xclip -selection clipboard
+    notify-send 'datatrash' 'upload complete'
 elif echo "$targets" | grep 'UTF8_STRING'; then
     xclip -selection clipboard -out -target 'UTF8_STRING' \
         | curl \
