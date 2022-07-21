22840f7c1ac28bc42712ce422c956c17f1b04df0
iyzana <16743652+iyzana@users.noreply.github.com>
2022-07-21 17:14:01 +0200

    rofi: add script to open intellij project

---
+++ .config/sway/rofi-jetbrains.sh
@@ -0,0 +1,19 @@
+#!/bin/env sh
+
+config_path="$HOME/.config/JetBrains/IntelliJIdea2022.1"
+recents_path="$config_path/options/recentProjects.xml"
+
+projects=$(xq '.application.component.option | .[] | select(.["@name"] == "additionalInfo").map.entry | .[] | { project: .["@key"], lastOpenend: .value.RecentProjectMetaInfo.option | .[] | select(.["@name"] == "projectOpenTimestamp") | .["@value"] }' "$recents_path")
+
+paths=$(echo "$projects" \
+    | jq --raw-output '.lastOpenend + " " + .project' \
+    | sort --reverse --numeric-sort \
+    | cut --fields 2 --delimiter=' ' \
+    | sed --expression "s#\\\$USER_HOME\\\$/projects/##" \
+    | grep --invert-match --fixed-strings "\$USER_HOME\$"
+)
+
+selected=$(echo "$paths" | rofi -dmenu)
+if [ -n "$selected" ]; then
+    swaymsg exec -- intellij-idea-ultimate-edition "$HOME/projects/$selected"
+fi
