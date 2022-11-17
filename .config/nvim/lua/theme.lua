f0c80099bdadc914a2cf34c0bf2b8e7c06076276
iyzana <16743652+iyzana@users.noreply.github.com>
2022-11-17 09:54:36 +0100

    vim: update nvim highlight calls

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -4,9 +4,9 @@ vim.opt.background = "dark"
 
 -- coc overrides
 local setCocColors = function()
-  vim.api.nvim_set_hl(0, "CocHighlightText", { bg = "#223922", underline = true, ctermbg = 2 })
+  vim.api.nvim_set_hl(0, "CocHighlightText", { bg = "#223922", underline = true })
   vim.api.nvim_set_hl(0, "CocInlayHint", { link = "Comment" })
-  -- vim.api.nvim_set_hl(0, "CocErrorSign", { fg = "fg" })
+  vim.api.nvim_set_hl(0, "CocErrorSign", { fg = "fg" })
   vim.api.nvim_set_hl(0, "CocFloatDividingLine", { link = "CocFloating" })
 end
 vim.api.nvim_create_autocmd({ "ColorScheme" }, { callback = setCocColors })
