8fc2af9d9f72d28c886548b1760060e92b673798
iyzana <16743652+iyzana@users.noreply.github.com>
2023-01-05 22:49:03 +0100

    sway: switch from rofi to tofi

--- home/.config/sway/scripts/rofi-jetbrains.sh
+++ home/.config/sway/scripts/rofi-jetbrains.sh
@@ -14,7 +14,7 @@ paths=$(echo "$projects" \
     | grep --invert-match --fixed-strings "\$USER_HOME\$"
 )
 
-selected=$(echo "$paths" | rofi -dmenu)
+selected=$(echo "$paths" | tofi --prompt-text "open:")
 if [ -n "$selected" ]; then
     swaymsg exec -- intellij-idea-ultimate-edition "$HOME/projects/$selected"
 fi
