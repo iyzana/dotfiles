e6f644f5a78d025d20208218a586e30e6defadd7
iyzana <16743652+iyzana@users.noreply.github.com>
2023-03-24 18:58:26 +0100

    sway: simplify ocr script

--- home/.config/sway/scripts/ocr.sh
+++ home/.config/sway/scripts/ocr.sh
@@ -2,16 +2,22 @@
 
 targets=$(wl-paste -l)
 if echo "$targets" | grep 'image/png'; then
-    image_file="/tmp/ocr-image-$(date --iso-8601=seconds)"
-    text_file="/tmp/ocr-text-$(date --iso-8601=seconds)"
-    wl-paste > "$image_file"
     languages=$(
-        find /usr/share/tessdata -maxdepth 1 -type f -name '*.traineddata' -print0 | xargs --null --replace=_ --max-args=1 basename _ .traineddata
+        find /usr/share/tessdata \
+            -maxdepth 1 \
+            -type f \
+            -name '*.traineddata' \
+            -print0 \
+            | xargs \
+            --null \
+            --replace=_ \
+            --max-args=1 \
+            basename _ .traineddata
     )
     lang=$(echo "$languages" | rofi -dmenu -p "language")
-    tesseract -l "$lang" "$image_file" "$text_file"
-    wl-copy < "$text_file.txt"
-    rm "$image_file" "$text_file.txt"
+    wl-paste \
+        | tesseract stdin stdout -l "$lang"  \
+        | wl-copy
     notify-send 'ocr' "$lang text copied to clipboard"
 else
     notify-send 'ocr' 'unsupported clipboard format'
